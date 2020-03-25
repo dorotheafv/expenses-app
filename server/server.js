ba9b3a7d4d29245cebdf75579319cff48e77b4f8
Dorothea <dwrafv@gmail.com>
2020-03-25 02:45:15 +0200

    server.js updated

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const publicPath = path.join(__dirname, '..','public');
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-    res.sendFile(publicPath, 'index.html');
+    res.sendFile(path.join(publicPath), 'index.html');
 });
 
 app.listen(port, () => {
